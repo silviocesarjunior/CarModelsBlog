a42537952251518c1c92d33f687b57715257fa0b
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2023-10-19 23:35:44 -0300

    projeto blog de carros

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,58 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Chevrolet Bolt EUV',
+    description:
+      'Além de um design moderno que contempla rodas de liga leve de 16” e grade frontal com detalhes cromados, o Onix 2024 te leva por qualquer caminho com muito estilo: os faróis dianteiros são tipo projetor e você ainda conta com luzes de condução diurna e lanternas em LED.',
+    photoCover:
+      'https://carros2023.com.br/wp-content/uploads/2023/05/Novo-Chevrolet-Bolt-EUV-Preco-mudancas-qualidades-e-vantagens-unicas.jpg',
+  },
+  {
+    id: '2',
+    title: 'Equinox EV',
+    description:
+      'A dianteira tem faróis afilados e integrados à grade. Atrás, a queda do teto é mais suave, ao estilo cupê, e as lanternas têm máscara negra e lente branca. O porte é menor do que o do Blazer. São 4,83 m de comprimento e 2,95 m de entre-eixos.',
+    photoCover:
+      'https://cdn.motor1.com/images/mgl/G3yvpJ/s1/2024-chevrolet-equinox-ev-exterior-front-quarter.webp',
+  },
+  {
+    id: '3',
+    title: 'Chevrolet Onix',
+    description:
+      'As versões topo de linha, Premier, voltam a ter ar-condicionado digital, alerta de objetos no ponto cego, Bluetooth, assistente de estacionamento semiautônomo e sensores de estacionamento dianteiros e laterais.',
+    photoCover:
+      'https://carros2024.com.br/wp-content/uploads/2023/04/chevrolet-onix-2024.jpg',
+  },
+  {
+    id: '4',
+    title: 'Montana cupê',
+    description:
+      'Compacto-médio, voltado a atuar no segmento de Jeep Compass, Toyota Corolla Cross e Volkswagen Taos.Trata-se de uma variante mais robusta da base GEM usada por Tracker, Onix e Onix Plus, voltada a veículos de porte um pouco maior e mais sofisticados',
+    photoCover:
+      'https://image1.mobiauto.com.br/images/api/images/v1.0/101280190/transform/',
+  },
+  {
+    id: '5',
+    title: 'Chevrolet Trax',
+    description:
+      'Mesmo sendo o carro mais barato da Chevrolet no mercado norte-americano, o Trax tem preço médio de venda de US$ 24.487 (R$ 115.750). Ou seja, os clientes não estão comprando unicamente a versão de entrada LS de US$ 20.400 (R$ 96.430), sem frete. O portfólio conta ainda com os acabamentos LT, RS e Activ, que podem superar os US$ 25.000 com frete e opcionais.',
+    photoCover:
+      'https://quatrorodas.abril.com.br/wp-content/uploads/2022/10/2024-chevrolet-trax-activ-001-e1665672028565.jpg?quality=70&strip=info&w=1280&h=720&crop=1',
+  },
+  {
+    id: '6',
+    title: 'Chevrolet Spin',
+    description:
+      'As mudanças mais radicais devem acontecer na grade dianteira, onde a Spin vai receber uma barra cromada inspirada no Tracker. O logotipo da Chevrolet será posicionado mais próximo do capô.A traseira deve manter características do modelo atual, estreando apenas novos grafismos para a iluminação. Até o formato das lanternas deve permanecer.',
+    photoCover:
+      'https://s2-autoesporte.glbimg.com/ZbKPXHhY5EQe2unAkRjPDP6_1vw=/0x0:3391x2004/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2021/d/r/LrSrMMTLyVvY1xmoCByA/orlandofrente.jpg',
+  },
+  {
+    id: '7',
+    title: 'Tracker RS',
+    description:
+      'A RS traz visual esportivo, com rodas de 17 polegadas escurecidas, faróis de LED com máscara negra, gravatinhas e plaqueta do nome do veículo da cor preta e apliques plásticos no para-choque traseiro.Considerando os equipamentos, o Tracker RS é um Tracker LTZ que subiu na vida e ganhou teto solar e faróis de LED. Fora isso, há seis airbags, alertas de veículos no ponto cego e de colisão frontal com frenagem automática de emergência, acesso por chave presencial, partida por botão, faróis de LED com acendimento automático, bancos de couro e central multimídia com tela de 8 polegadas e conexões Android Auto e Apple CarPlay sem fio.',
+    photoCover:
+      'https://www.autossegredos.com.br/wp-content/webp-express/webp-images/uploads/2022/05/Chevrolet-Tracker-RS-4-696x418.jpeg.webp',
+  },
+];
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css'],
+})
+export class ContentComponent implements OnInit {
+  @Input()
+  photoCover: string =
+    'https://carros2024.com.br/wp-content/uploads/2023/04/chevrolet-onix-2024.jpg';
+  @Input()
+  contentTitle: string = '';
+  @Input()
+  contentDescription: string = '';
+  private id: string | null = '0';
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id == id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
+  }
+}
